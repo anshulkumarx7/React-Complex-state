266460dddf306e5f8b7adb987fc5fa2fdca4bedc
anshulkumarx7 <anshulkumarx7@gmail.com>
2023-01-27 20:40:29 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [fullName, setfullName] = useState({
+    fName: "",
+    lName: "",
+  });
+  const [isSubmitted,setSubmit]=useState(false);
+  function handleChange(event) {
+    const {value,name}=event.target;
+    setfullName(prevValue=>{
+      if(name === "fName"){
+        return {
+          fName:value,
+          lName:prevValue.lName
+        }
+      }
+      else if(name === "lName"){
+        return {
+          fName:prevValue.fName,
+          lName:value
+        }
+      }
+    })
+  };
+  function handleClick(e){
+    e.preventDefault();
+    console.log("clicked");
+    setSubmit(true);
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+    {isSubmitted ? <h1>Submitted Sucessfully</h1>:
+    <div>
+    <h1>
+        Hello {fullName.fName} {fullName.lName}
+      </h1>
+      <form onSubmit={handleClick}>
+        <input
+          name="fName"
+          onChange={handleChange}
+          placeholder="First Name"
+          value={fullName.fName}
+        />
+        <input
+          name="lName"
+          onChange={handleChange}
+          placeholder="Last Name"
+          value={fullName.lName}
+        />
+        <button>Submit</button>
+        </form>
+      </div>  
+    }
+      
+    </div>    
   );
 }
 
